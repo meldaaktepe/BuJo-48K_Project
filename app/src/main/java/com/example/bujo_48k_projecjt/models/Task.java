0c4d34ac3e138894541536cdd3e966e7acc30431
bekorn <berkcanli@sabanciuniv.edu>
2019-12-07 14:33:48 +0300

    Adds basic models

---
+++ app/src/main/java/com/example/bujo_48k_projecjt/models/Task.java
@@ -0,0 +1,11 @@
+package com.example.bujo_48k_projecjt.models;
+
+import java.util.Date;
+
+public class Task
+{
+    private String title;
+    private TaskClass taskClass;
+    private Date dueDate;
+    private String description;
+}
---
+++ app/src/main/java/com/example/bujo_48k_projecjt/models/TaskClass.java
@@ -0,0 +1,10 @@
+package com.example.bujo_48k_projecjt.models;
+
+import android.graphics.Color;
+
+class TaskClass
+{
+    private int importance;
+    private String emoji;
+    private Color color;
+}
